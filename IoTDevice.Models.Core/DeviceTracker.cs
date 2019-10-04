796ecfbddf8d62130443522339921182dfcd09f0
snm <m.sudharsanam18@gmail.com>
2019-10-04 12:53:49 +0530

    Update DeviceTracker.cs

--- IoTDevice.Models.Core/DeviceTracker.cs
+++ IoTDevice.Models.Core/DeviceTracker.cs
@@ -9,7 +9,6 @@ namespace IoTDevice.Models.Core
         public int Id { get; set; }//DeviceTracker Id
         public int DeviceId { get; set; }//Device.Id
         public DateTimeOffset CreatedDate { get; set; }
-        public string Name { get; set; } //Peroperty Name or Key i.e. Heart rate
-        public string Value { get; set; } //Property value i.e. 120
+        public string Data { get; set; } //JSON Data i.e. "{ "Heart rate" : 120 }"         
     }
 }
