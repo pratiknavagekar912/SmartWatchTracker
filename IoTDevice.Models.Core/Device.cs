66422f9bab3bd8cdfa6332155eb46e21d2f5e2aa
snm <m.sudharsanam18@gmail.com>
2019-10-04 12:00:16 +0530

    Update Device.cs
    - RegId and MACId columns

--- IoTDevice.Models.Core/Device.cs
+++ IoTDevice.Models.Core/Device.cs
@@ -8,7 +8,9 @@ namespace IoTDevice.Models.Core
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Type { get; set; }//DeviceType.TypeId
+        public int Type { get; set; }//DeviceType.TypeId        
+        public string MacId { get; set; } //Device unqiue MAC id 
+        public string RegistrationId { get; set; } //Unique Guid        
         public DateTimeOffset RegistrationTime { get; set; }
     }
 }
